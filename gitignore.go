9864b60dc874b2248794cb63eec0d829b50ec46f
Brandon LeBlanc <brandon@platform.sh>
2019-03-17 18:32:11 -0500

    move makeGitignore to the gitignore file

--- gitignore.go
+++ gitignore.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"path"
+	"strings"
+
+	"github.com/google/go-github/v24/github"
+)
+
 // Gitignore represents a gitignore template from the repository
 type Gitignore struct {
 	Name string
@@ -9,4 +16,32 @@ type Gitignore struct {
 	SHA  string
 
 	state *State
+}
+
+func (s *State) makeGitignore(entry github.TreeEntry) *Gitignore {
+	Size := uint64(entry.GetSize())
+	Path := entry.GetPath()
+	SHA := entry.GetSHA()
+
+	if strings.HasSuffix(Path, Suffix) {
+		dir, basename := path.Split(Path)
+		Name := strings.TrimSuffix(basename, Suffix)
+		if Name == "" {
+			return nil
+		}
+
+		dir = strings.TrimRight(dir, "/")
+		Tags := strings.Split(dir, "/")
+
+		return &Gitignore{
+			Name,
+			Size,
+			Path,
+			Tags,
+			SHA,
+			s,
+		}
+	}
+
+	return nil
 }
--- state.go
+++ state.go
@@ -196,31 +196,3 @@ func (s *State) getTree(sha string) map[string]*Gitignore {
 
 	return rv
 }
-
-func (s *State) makeGitignore(entry github.TreeEntry) *Gitignore {
-	Size := uint64(entry.GetSize())
-	Path := entry.GetPath()
-	SHA := entry.GetSHA()
-
-	if strings.HasSuffix(Path, Suffix) {
-		dir, basename := path.Split(Path)
-		Name := strings.TrimSuffix(basename, Suffix)
-		if Name == "" {
-			return nil
-		}
-
-		dir = strings.TrimRight(dir, "/")
-		Tags := strings.Split(dir, "/")
-
-		return &Gitignore{
-			Name,
-			Size,
-			Path,
-			Tags,
-			SHA,
-			s,
-		}
-	}
-
-	return nil
-}
